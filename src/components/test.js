486534a2083204670b62f151a56d65ae086c0810
angelo-inexa <nlagni28@gmail.com>
2021-05-12 09:49:42 +0000

    implement firebase utilis, included ability to store  authentication in firestore database

---
+++ src/components/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('PipvBiGn3oMXYNBtjhWL').collection('cartItems').doc('IhH2vnlXT5lKxc9Aojwy');
+firestore.doc('/users/PipvBiGn3oMXYNBtjhWL/cartItems/IhH2vnlXT5lKxc9Aojwy');
+firestore.collection('/users/PipvBiGn3oMXYNBtjhWL/cartItems');
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,7 +10,30 @@ const config = {
     messagingSenderId: "6306417363",
     appId: "1:6306417363:web:0e5a7cefac34ec41dae226"
   };
+  export const createUserProfileDocument = async (userAuth, additionalData)=>{
+      if(!userAuth) return ;
+    
+      const userRef = firestore.doc(`users/${userAuth.uid}`);
+      const snapShot = await userRef.get();
+      console.log(snapShot) ;
 
+      if(!snapShot.exists){
+          const { displayName, email } = userAuth;
+          const createdAt = new Date();
+
+          try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+          }catch(error){
+            console.log('error creating user ', error.message);
+          }
+      }
+      return userRef;
+  }
   firebase.initializeApp(config);
   
   export const auth = firebase.auth();
